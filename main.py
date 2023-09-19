5c44a8a234417aa4a07afcfdbec9533fb70d426c
okorneev <okorneev42@gmail.com>
2023-09-19 23:11:52 +0300

    555

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ try:
     with  connect(
         host="localhost",
         user=input("Введите имя пользователя: "),
-        password=getpass("Введите пароль:"),
+        password=getpass("Введите парhhhоль:"),
     ) as connection:
         show_db_query = "SHOW DATABASES"
         with connection.cursor() as cursor:
